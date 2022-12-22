a5693e7e959045f697802f60799a62b7fa8972a2
Ngoc Tran <tranthainhien1603@gmail.com>
2022-12-22 18:53:11 +0200

    complete part 1 courseinfo step1

---
+++ part 1/courseinfo/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Header = (props) =>{
+  return (
+    <p>(props.course)</p>
+  )
+}
+const Content = (props) =>{
+  return (
+    <p>(props.part) (props.exc)</p>
+  )
+}
+const Total = (props) =>{
+  return (
+    <p>(props.ex1)+(props.ex2)+ (props.ex3)</p>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exc={exercises1}/>
+      <Content part={part2} exc={exercises2}/>
+      <Content part={part3} exc={exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+
+    </div>
+  );
+}
+
+export default App;
