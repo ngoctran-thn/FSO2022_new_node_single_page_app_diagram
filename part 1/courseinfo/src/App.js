8db8a9e36ae8ca66e33e96d09b2953bcca4a12f8
Ngoc Tran <tranthainhien1603@gmail.com>
2022-12-22 19:34:48 +0200

    fix part 1 courseinfo step

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -1,37 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 const Header = (props) =>{
   return (
-    <p>(props.course)</p>
+  <p>{props.course}</p> 
   )
+    
+  
+  
 }
 const Part = (props) =>{
   return (
-    <p>(props.part) (props.exc)</p>
+    <p>{props.part} {props.exc}</p>
   )
 }
 
-
-
 const Total = (props) =>{
   return (
-    <p>(props.ex1)+(props.ex2)+ (props.ex3)</p>
+    <p>{props.ex1}+{props.ex2}+ {props.ex3}</p>
   )
 }
 const App = () => {
   const course = 'Half Stack application development'
-  
-  return (
-    <div>
-      <Header course={course}/>
-      
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
 
-    </div>
-  );
-}
-const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -40,9 +30,22 @@ const Content = () => {
   const exercises3 = 14
   return (
     <div>
-    <Part part={part1} exc={exercises1}/>
-    <Part part={part2} exc={exercises2}/>
-    <Part part={part3} exc={exercises3}/>
+      <Header course={course}/>
+      <Content part1 ={part1} exc1={exercises1} 
+      part2 ={part2} exc2={exercises2} part3 ={part3} exc3={exercises3}/>
+      
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+
+    </div>
+  );
+}
+const Content = (props) => {
+  
+  return (
+    <div>
+    <Part part={props.part1} exc={props.exc1}/>
+    <Part part={props.part2} exc={props.exc2}/>
+    <Part part={props.part3} exc={props.exc3}/>
     </div>
   )
   }
