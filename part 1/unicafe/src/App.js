5bc12cc94cb7bcb70fd49c03f951e4a0f73ba97a
Ngoc Tran <tranthainhien1603@gmail.com>
2022-12-24 00:06:34 +0200

    part 1 unicafe step 1.1

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -5,10 +5,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const goodHandler =()=>{
+    setGood(good +1);
+  }
+  const neutralHandler =()=>{
+    setNeutral(neutral +1);
+  }
+  const badHandler =()=>{
+    setBad(bad +1);
+  }
 
   return (
     <div>
-      code here
+      <h2>Give feedback</h2>
+      <button onClick={goodHandler}> Good</button>
+      <button onClick={neutralHandler}> Neutral</button>
+      <button onClick={badHandler}> Bad</button>
+      <p> Good: {good} </p><br/>
+      <p> Neutral: {neutral} </p><br/>
+      <p> Bad: {bad} </p><br/>
     </div>
   )
 }
