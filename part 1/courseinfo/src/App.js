6b12139f7e8ebf95f6c8718755e872745b0b0c5c
Ngoc Tran <tranthainhien1603@gmail.com>
2022-12-23 12:30:14 +0200

    part 1 courseinfo step3

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -21,20 +21,26 @@ const Total = (props) =>{
 }
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
   return (
     <div>
       <Header course={course}/>
-      <Content part1 ={part1} exc1={exercises1} 
-      part2 ={part2} exc2={exercises2} part3 ={part3} exc3={exercises3}/>
+      <Content part1 ={part1.name} exc1={part1.exercises} 
+      part2 ={part2.name} exc2={part2.exercises} part3 ={part3.name} exc3={part3.exercises}/>
       
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+      <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises}/>
 
     </div>
   );
