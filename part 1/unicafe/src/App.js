0ff17ae755d06374d67daba29d9c0a235f14c670
Ngoc Tran <tranthainhien1603@gmail.com>
2022-12-24 00:14:24 +0200

    part 1 unicafe step2

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -14,6 +14,12 @@ const App = () => {
   const badHandler =()=>{
     setBad(bad +1);
   }
+  const average =(good,neutral,bad)=>{
+    return (good-bad)/(good+neutral+bad)
+  }
+  const positive =(good,neutral,bad)=>{
+    return good/(good+neutral+bad)
+  }
 
   return (
     <div>
@@ -21,9 +27,12 @@ const App = () => {
       <button onClick={goodHandler}> Good</button>
       <button onClick={neutralHandler}> Neutral</button>
       <button onClick={badHandler}> Bad</button>
+
       <p> Good: {good} </p><br/>
       <p> Neutral: {neutral} </p><br/>
       <p> Bad: {bad} </p><br/>
+      <p> average: {average(good,neutral,bad)}</p><br/>
+      <p> positive: {positive(good,neutral,bad)}</p><br/>
     </div>
   )
 }
