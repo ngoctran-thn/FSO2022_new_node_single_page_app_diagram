5e95d45e7c33629af7e6f0de44b1f3a695aeaccc
Ngoc Tran <tranthainhien1603@gmail.com>
2023-01-04 15:52:56 +0200

    extracting into Filer component

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,13 +1,40 @@
 import { useState } from "react";
 
-const Numbers = (props) => {
-  return <p>{props.name} - {props.phone}</p>;
+const Persons = (props) => {
+  return (
+    <p>
+      {props.name} - {props.phone}
+    </p>
+  );
 };
+
+const Filter = (props) => {
+  const [searchString, setSearchString] = useState("");
+
+  const handleSearchString = (event) => {
+    // console.log(event.target.value);
+    const { value: currentSearchString } = event.target;
+    setSearchString(currentSearchString);
+
+    props.onFilter(currentSearchString);
+  };
+
+  return (
+    <div>
+      Filter shown with :{" "}
+      <input />
+      <br />
+    </div>
+  );
+};
+
+const PersonForm = () => {};
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [searchString, setSearchString] = useState("");
+  const [personToShow, setPersonToShow] = useState([]);
   const [messgae, setMessage] = useState("");
 
   const addNameHandler = () => {
@@ -17,6 +44,7 @@ const App = () => {
       const newPersons = [...persons];
       newPersons.push({ name: newName, number: newNumber });
       setPersons(newPersons);
+      setPersonToShow(newPersons)
     }
     setNewName("");
     setNewNumber("");
@@ -33,22 +61,20 @@ const App = () => {
     // console.log(event.target.value);
     setNewNumber(event.target.value);
   };
-  const handleSearchString = (event) => {
-    // console.log(event.target.value);
-    const {value:currentSearchString} = event.target
-    setSearchString(currentSearchString);
+
+  
+  const onFilter = (searchString) => {
+    setPersonToShow(
+      searchString === ""
+        ? persons
+        : persons.filter((p) => p.name.includes(searchString))
+    );
   };
-  const personToShow = 
-  searchString === ""
-  ? persons
-  : persons.filter(p => p.name.includes(searchString))
 
   return (
     <div>
       <h2>Phonebook</h2>
-      Filter shown with :{" "}
-      <input value={searchString} onChange={handleSearchString} />
-      <br />
+      <Filter onFilter={onFilter} />
       <h2> Add a new</h2>
       name: <input value={newName} onChange={handleNameChange} />
       <br />
@@ -63,7 +89,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {personToShow.map((person) => (
-          <Numbers key={person.name} name={person.name} phone={person.number} />
+          <Persons key={person.name} name={person.name} phone={person.number} />
         ))}
       </ul>
       ...
