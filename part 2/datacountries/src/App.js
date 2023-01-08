059d2b2caaaaa3d29bc8c5f8f974b5ab6086c4c0
Ngoc Tran <tranthainhien1603@gmail.com>
2023-01-08 22:27:51 +0200

    part 2 datacountries filter countries and limit result less than 10

--- part 2/datacountries/src/App.js
+++ part 2/datacountries/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const Country = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> {props.name} </p>
+    </div>
+  );
+};
+
+const Filter = (props) => {
+  const [searchString, setSearchString] = useState("");
+  const handleSearchString = (event) => {
+    const { value: currentSearchString } = event.target;
+    setSearchString(currentSearchString);
+    props.onFilter(currentSearchString);
+  };
+
+  return (
+    <div>
+      Find countries <br />{" "}
+      <input value={searchString} onChange={handleSearchString} />
+    </div>
+  );
+};
+function App() {
+  const [countries, setContries] = useState([]);
+  const [countriesToshow, setCountriesToShow] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setContries(data);
+        setCountriesToShow(data);
+      });
+  }, []);
+
+  console.log(countries);
+
+  const onFilter = (searchString) => {
+    setQuery(searchString);
+    setCountriesToShow(
+      searchString === ""
+        ? countries
+        : countries.filter((c) => c.name.common.includes(searchString))
+    );
+  };
+  return (
+    <div>
+      <Filter onFilter={onFilter} />
+      <div>
+        {countriesToshow.length > 10 && query != "" ? (
+          <div> Too many matches, specify another filter </div>
+        ) : (
+          countriesToshow.map((c) => (
+            <Country key={c.capital} name={c.name.common} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
